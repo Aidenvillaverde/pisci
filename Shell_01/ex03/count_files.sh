dc5c5f86daec836e4f0eba8bcab2d3e748da3b39
Aidenvillaverde <133431264+Aidenvillaverde@users.noreply.github.com>
2023-06-02 20:26:40 +0200

    bankai

--- Shell_01/ex03/count_files.sh
+++ Shell_01/ex03/count_files.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 find . | wc -l | tr -d " "
-
 # find  -> comando para buscar
 # .     -> el . indica que buscaremos a partir de todo lo que tenemos por debajo
 # wc -l	->  cuenta el numero de lineas
